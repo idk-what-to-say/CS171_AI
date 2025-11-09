0db3384afb159485871c5ec7d581f2acd3a584c8
Yinuo Peng <yinuop2@uci.edu>
2025-11-08 21:51:22 -0800

    test

--- src/checkers-python/StudentAI.py
+++ src/checkers-python/StudentAI.py
@@ -15,13 +15,5 @@ class StudentAI():
         self.opponent = {1:2,2:1}
         self.color = 2
     def get_move(self,move):
-        if len(move) != 0:
-            self.board.make_move(move,self.opponent[self.color])
-        else:
-            self.color = 1
-        moves = self.board.get_all_possible_moves(self.color)
-        index = randint(0,len(moves)-1)
-        inner_index =  randint(0,len(moves[index])-1)
-        move = moves[index][inner_index]
-        self.board.make_move(move,self.color)
+
         return move
